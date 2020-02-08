2ffa0c6b94c8a4a7e60e2d77e06d38519e64d613
Vinayak Kamath <58919830+kamathvk1982@users.noreply.github.com>
2020-02-08 14:49:30 -0500

    Add files via upload

--- GenerateMoviesRatingsCSV.sql
+++ GenerateMoviesRatingsCSV.sql
@@ -7,25 +7,31 @@
 /* SHOW VARIABLES LIKE "secure_file_priv"; */
 /* Pls ensure no files exists in the outfile path with names as movies.csv, users.csv and ratings.csv */
 
-SELECT * INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/movies.csv'
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-ESCAPED BY '\\'
-LINES TERMINATED BY '\n'
+
+SELECT 'Id','MovieName','YearOfRelease','Genre'
+UNION
+SELECT movie_id, name, year, genre
 FROM movies
-ORDER BY movie_id;
+INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/movies.csv'
+FIELDS TERMINATED BY  ','
+ENCLOSED BY  '"' ;
 
-SELECT * INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users.csv'
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-ESCAPED BY '\\'
-LINES TERMINATED BY '\n'
+
+SELECT 'Id','UserName','Age'
+UNION
+SELECT user_id, name, age
 FROM users
-ORDER BY user_id;
+INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users.csv'
+FIELDS TERMINATED BY  ','
+ENCLOSED BY  '"';
 
-SELECT r.movie_id movie_id, m.name movie_name,  r.user_id user_id, u.name user_name, rating INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ratings.csv'
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-ESCAPED BY '\\'
-LINES TERMINATED BY '\n'
+
+SELECT 'Ratings','MovieId','MovieName','UserId','Username'
+UNION
+SELECT COALESCE(r.rating,''), r.movie_id movie_id, m.name movie_name,  r.user_id user_id, u.name user_name 
 FROM users u , movies m, ratings r
 where u.user_id = r.user_id
   and m.movie_id = r.movie_id
-ORDER BY user_id;
+INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ratings.csv'
+FIELDS TERMINATED BY  ','
+ENCLOSED BY  '"';
