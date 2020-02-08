5537aa83fe3ac4a642233507ed3c1d20e110aff0
Vinayak Kamath <58919830+kamathvk1982@users.noreply.github.com>
2020-02-08 15:49:23 -0500

    Add files via upload

--- GenerateMoviesRatingsCSV.sql
+++ GenerateMoviesRatingsCSV.sql
@@ -1,37 +1,44 @@
 /*
   GenerateMoviesRatingsCSV.sql
-  Task:  export .CSV file that contains the data for movies[movies.csv], users[users.csv], and ratings[ratings.csv].
+  Task:  Export .CSV file that contains the data for movies[movies.csv], users[users.csv], ratings[ratings.csv] and upcoming_movies[upcoming_movies.csv].
+  Create Dt: 7th FEb 2020			Created By: Vinayak K
+  Change Dt: 						Changed By: 
 */
 
 /* Pls run below command to get the outfile path*/
 /* SHOW VARIABLES LIKE "secure_file_priv"; */
-/* Pls ensure no files exists in the outfile path with names as movies.csv, users.csv and ratings.csv */
+/* Pls ensure no files exists in the outfile path with names as movies.csv, users.csv ,ratings.csv and upcoming_movies.csv*/
 
 
-SELECT 'Id','MovieName','YearOfRelease','Genre'
-UNION
 SELECT movie_id, name, year, genre
 FROM movies
 INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/movies.csv'
-FIELDS TERMINATED BY  ','
-ENCLOSED BY  '"' ;
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n' ;
 
 
-SELECT 'Id','UserName','Age'
-UNION
 SELECT user_id, name, age
 FROM users
 INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users.csv'
-FIELDS TERMINATED BY  ','
-ENCLOSED BY  '"';
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n';
 
 
-SELECT 'Ratings','MovieId','MovieName','UserId','Username'
-UNION
-SELECT COALESCE(r.rating,''), r.movie_id movie_id, m.name movie_name,  r.user_id user_id, u.name user_name 
+SELECT r.movie_id movie_id, m.name movie_name,  r.user_id user_id, u.name user_name , coalesce( r.rating, '')
 FROM users u , movies m, ratings r
 where u.user_id = r.user_id
   and m.movie_id = r.movie_id
 INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ratings.csv'
-FIELDS TERMINATED BY  ','
-ENCLOSED BY  '"';
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n';
+
+
+SELECT  movie_id, name, year, genre, auto_rating
+FROM upcoming_movies
+INTO OUTFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/upcoming_movies.csv'
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n';
--- movies.sql
+++ movies.sql
@@ -1,8 +1,11 @@
--- MySQL 
---
--- Host: localhost    Database: movies
--- ------------------------------------------------------
-
+/*
+  movies.sql
+  Task:  Create table and load data for movies, users, ratings and upcoming_movies tables in database movies.
+  MySQL
+  Host: localhost    				Database: movies
+  Create Dt: 7th FEb 2020			Created By: Vinayak K
+  Change Dt: 						Changed By: 
+*/
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -67,6 +70,24 @@ CREATE TABLE `ratings` (
 
 
 --
+-- Table structure for table `upcoming_movies`
+--
+
+DROP TABLE IF EXISTS `upcoming_movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `upcoming_movies` (
+  `movie_id` integer(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `genre` varchar(35) NOT NULL,
+  `auto_rating` integer(1) ,
+  PRIMARY KEY (`movie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
 -- Dumping data for table `movies`
 --
 
@@ -81,6 +102,8 @@ INSERT INTO `movies` VALUES (6,'Parasite','2019','Comedy');
 
 
 UNLOCK TABLES;
+
+
 
 --
 -- Dumping data for table `users`
@@ -112,6 +135,26 @@ INSERT INTO `ratings` VALUES (1,4,5);INSERT INTO `ratings` VALUES (2,4,NULL);INS
 INSERT INTO `ratings` VALUES (1,5,3);INSERT INTO `ratings` VALUES (2,5,5);INSERT INTO `ratings` VALUES (3,5,4);INSERT INTO `ratings` VALUES (4,5,5);INSERT INTO `ratings` VALUES (5,5,3);INSERT INTO `ratings` VALUES (6,5,NULL);
 INSERT INTO `ratings` VALUES (1,6,NULL);INSERT INTO `ratings` VALUES (2,6,NULL);INSERT INTO `ratings` VALUES (3,6,4);INSERT INTO `ratings` VALUES (4,6,4);INSERT INTO `ratings` VALUES (5,6,NULL);INSERT INTO `ratings` VALUES (6,6,NULL);
 INSERT INTO `ratings` VALUES (1,7,5);INSERT INTO `ratings` VALUES (2,7,NULL);INSERT INTO `ratings` VALUES (3,7,4);INSERT INTO `ratings` VALUES (4,7,4);INSERT INTO `ratings` VALUES (5,7,5);INSERT INTO `ratings` VALUES (6,7,NULL);
+
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `upcoming_movies`
+--
+
+LOCK TABLES `upcoming_movies` WRITE;
+
+INSERT INTO `upcoming_movies` VALUES (1,'My Spy','2020','Comedy', 3);
+INSERT INTO `upcoming_movies` VALUES (2,'Inside the Rain','2020','Comedy', 4);
+INSERT INTO `upcoming_movies` VALUES (3,'Military Wives','2020','Comedy', 5);
+INSERT INTO `upcoming_movies` VALUES (4,'No Time to Die','2020','Action', 5);
+INSERT INTO `upcoming_movies` VALUES (5,'Charm City Kings','2020','Drama', 3);
+INSERT INTO `upcoming_movies` VALUES (6,'Mulan','2020','Action', 3);
+INSERT INTO `upcoming_movies` VALUES (7,'Black Widow','2020','Action', 4);
+INSERT INTO `upcoming_movies` VALUES (8,'Bobs Burgers: The Movie','2020','Comedy',2);
+INSERT INTO `upcoming_movies` VALUES (9,'Wonder Woman 1984','2020','Action', 4);
+INSERT INTO `upcoming_movies` VALUES (10,'The Kings Man','2020','Comedy', 2);
 
 UNLOCK TABLES;
 
